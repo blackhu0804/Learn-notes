0a720e626d30421f2795ff80c133fd96a4f951bb
hu970804 <812510003@qq.com>
2017-09-02 15:58:00 +0800

    add

---
+++ task/senior-task/task5/demo0/index.js
@@ -0,0 +1,6 @@
+var marked = require('marked');
+
+var str = marked('# hello world');
+sonsole.log(str);
+
+module.exports = str;
---
+++ task/senior-task/task5/demo1/index.js
@@ -0,0 +1,3 @@
+var marked = require('marked');
+var str = marked('# hello');
+console.log(str);
